ddacf39fdd1179e19ec55aafd40ba64160dacccc
Jeremiah Silva <jeremiah.ca9@gmail.com>
2022-08-24 13:30:59 -0600

    Created Listeners

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import './header.css';
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import Jeremiah from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h4> Hello I'm</h4>
+        <h1>Jeremiah Silva</h1>
+        <h4 className='text-light'> Fullstack Developer</h4>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={Jeremiah} alt='Myself' />
+        </div>
+          <a href='#contact' className='scroll__down'>Scroll Down</a>
+      </div> 
+    </header>
   )
 }
 
