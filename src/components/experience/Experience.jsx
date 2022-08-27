7cbe909fcd352e799c4b9e11eae0338875021639
Jeremiah Silva <jeremiah.ca9@gmail.com>
2022-08-27 01:23:21 -0600

    Updated Experience Component

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -6,41 +6,42 @@ const Experience = () => {
   return (
     <section id='experience'>
       <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
       
       <div className="container experience__container">
         <div className="experience__frontend">
           <h3>Frontend Development</h3>
           <div className="experience__content">
               <article className='experience__details'>
-              <BsCheckCircleFill />
+              <BsCheckCircleFill className='experience__details-icon'/>
             <div>
               <h2>HTML</h2>
               <medium className='text-light'>Experienced</medium>
             </div>
             </article>
             <article className='experience__details'>
-             <BsCheckCircleFill />
+             <BsCheckCircleFill className='experience__details-icon'/>
              <div>
              <h2>CSS</h2>
              <medium className='text-light'>Experienced</medium>
              </div>
             </article>
             <article className='experience__details'>
-            <BsCheckCircleFill />
+            <BsCheckCircleFill className='experience__details-icon'/>
             <div>
             <h2>JavaScript</h2>
             <medium className='text-light'>Experienced</medium>
             </div>
             </article>
             <article className='experience__details'>
-            <BsCheckCircleFill />
+            <BsCheckCircleFill className='experience__details-icon'/>
             <div>
             <h2>ReactJS</h2>
             <medium className='text-light'>Experienced</medium>
             </div>
             </article>
             <article className='experience__details'>
-            <BsCheckCircleFill />
+            <BsCheckCircleFill className='experience__details-icon'/>
             <div>
             <h2>VueJS</h2>
             <medium className='text-light'>Experienced</medium>
@@ -54,28 +55,28 @@ const Experience = () => {
         <h3>Backend Development</h3>
           <div className="experience__content">
             <article className='experience__details'>
-            <BsCheckCircleFill />
+            <BsCheckCircleFill className='experience__details-icon'/>
             <div>
             <h2>NodeJs</h2>
             <medium className='text-light'>Experienced</medium>
             </div>
             </article>
             <article className='experience__details'>
-            <BsCheckCircleFill />
+            <BsCheckCircleFill className='experience__details-icon'/>
             <div>
             <h2>MongoDB</h2>
             <medium className='text-light'>Experienced</medium>
             </div>
             </article>
             <article className='experience__details'>
-            <BsCheckCircleFill />
+            <BsCheckCircleFill className='experience__details-icon'/>
             <div>
             <h2>SQL</h2>
             <medium className='text-light'>Experienced</medium>
             </div>
             </article>
             <article className='experience__details'>
-            <BsCheckCircleFill />
+            <BsCheckCircleFill className='experience__details-icon'/>
             <div>
             <h2>Phyton</h2>
             <medium className='text-light'>Experienced</medium>
