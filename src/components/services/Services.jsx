744b4c59f82b63aa6e8aa761cacf37c8352e8846
Jeremiah Silva <jeremiah.ca9@gmail.com>
2022-08-29 22:14:37 -0600

    Updated File

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,107 @@
 import React from 'react';
+import './services.css';
+import {BiCheck} from 'react-icons/bi';
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+          <h3>UI/UX Design</h3>
+          </div> 
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>bla bla bla bla abncjfjr 91923ksk skjds</p> 
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>bla bla bla bla abncjfjr 91923ksk skjds</p> 
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>bla bla bla bla abncjfjr 91923ksk skjds</p> 
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>bla bla bla bla abncjfjr 91923ksk skjds</p> 
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>bla bla bla bla abncjfjr 91923ksk skjds</p> 
+            </li>
+          </ul>
+      </article>  
+      {/* END OF UI/UX*/}
+      <article className="service">
+          <div className="service__head">
+          <h3>Web Development</h3>
+          </div> 
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>bla bla bla bla abncjfjr 91923ksk skjds</p> 
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>bla bla bla bla abncjfjr 91923ksk skjds</p> 
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>bla bla bla bla abncjfjr 91923ksk skjds</p> 
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>bla bla bla bla abncjfjr 91923ksk skjds</p> 
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>bla bla bla bla abncjfjr 91923ksk skjds</p> 
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>bla bla bla bla abncjfjr 91923ksk skjds</p> 
+            </li>
+          </ul>
+      </article>  
+      {/* WEB DEVELOPMENT*/}
+      <article className="service">
+          <div className="service__head">
+          <h3>Define the matter</h3>
+          </div> 
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>bla bla bla bla abncjfjr 91923ksk skjds</p> 
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>bla bla bla bla abncjfjr 91923ksk skjds</p> 
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>bla bla bla bla abncjfjr 91923ksk skjds</p> 
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>bla bla bla bla abncjfjr 91923ksk skjds</p> 
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>bla bla bla bla abncjfjr 91923ksk skjds</p> 
+            </li>
+          </ul>
+      </article>  
+      {/* END OF DEFINE THE MATTER*/}
+      </div>
+      
+      </section>
   )
 }
 
