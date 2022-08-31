8b0b60bfc98bee0cf53b2044ffa6835568bfb9f4
Jeremiah Silva <jeremiah.ca9@gmail.com>
2022-08-31 12:03:15 -0600

    Created two more itens

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,7 +5,9 @@ import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
 import {BiMessageSquareDetail} from 'react-icons/bi'
+import {BiSlideshow} from 'react-icons/bi'
 import {useState} from 'react'
+import {RiUserShared2Line} from 'react-icons/ri'
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
@@ -15,6 +17,8 @@ const Nav = () => {
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
       <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><BiSlideshow/></a>
+      <a href="#testimonials" onClick={() => setActiveNav('#testimonials')} className={activeNav === '#testimonials' ? 'active' : ''}><RiUserShared2Line/></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
     </nav>
   )
