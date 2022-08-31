e462fed7ddfb38a27c8b071b72499f00f3cde9e5
Jeremiah Silva <jeremiah.ca9@gmail.com>
2022-08-31 12:20:09 -0600

    Created Target

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,7 +5,7 @@ import Resume from '../../assets/Jeremiah Resume.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={Resume} className='btn'>Download Resume</a>
+        <a href={Resume} target='_blank'className='btn'>Download Resume</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
