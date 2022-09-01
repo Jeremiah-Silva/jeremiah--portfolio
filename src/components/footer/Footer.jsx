185659dc8efd5af61566b1000844d2e529796911
Jeremiah Silva <jeremiah.ca9@gmail.com>
2022-08-31 23:10:43 -0600

    Updating File

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,24 @@ import './footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Jeremiah</a>
+        <ul className='permalinks'>
+          <li><a href="#">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#experience">Experience</a></li>
+          <li><a href="#services">Sercives</a></li>
+          <li><a href="#portfolio">PortFolio</a></li>
+          <li><a href="#testimonials">Testimonials</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+
+        <div className="footer__socials">
+          <a href='https://facebook.com'></a>
+          <a href='https://instagram.com'></a>
+          <a href='https://twitter.com'></a>
+        </div>
+      </footer>
   )
 }
 
