197adef8b12052ed2eb63a17f0996f4ddf562cc4
Jeremiah Silva <jeremiah.ca9@gmail.com>
2022-09-01 10:00:21 -0600

    Updated File

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,8 +6,8 @@ import {FiDribbble} from 'react-icons/fi'
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href='https://linkedin.com' target="_blank"><BsLinkedin /></a>
-        <a href='https://github.com' target="_blank"><FaGithub /></a>   
+        <a href='https://www.linkedin.com/in/jeremiah-silva-b29aba213/' target="_blank"><BsLinkedin /></a>
+        <a href= 'https://github.com/Jeremiah-Silva' target="_blank"><FaGithub /></a>   
         <a href='https:/dribbble.com' target="_blank"><FiDribbble /></a>
     </div>
   )
