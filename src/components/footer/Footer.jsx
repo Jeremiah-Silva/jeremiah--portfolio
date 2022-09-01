143cf7c4bff30efb5d6aa300ed4488c809bab233
Jeremiah Silva <jeremiah.ca9@gmail.com>
2022-08-31 23:36:23 -0600

    Updated File

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ import { IoLogoTwitter } from 'react-icons/io'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>Jeremiah</a>
+      <a href="#" className='footer__logo'>Jeremiah Silva</a>
         <ul className='permalinks'>
           <li><a href="#">Home</a></li>
           <li><a href="#about">About</a></li>
@@ -22,6 +22,10 @@ const Footer = () => {
           <a href='https://facebook.com'><FaFacebookF/></a>
           <a href='https://instagram.com'><FiInstagram/></a>
           <a href='https://twitter.com'><IoLogoTwitter/></a>
+        </div>
+
+        <div className="footer__copyright">
+          <small>&copy; Jeremiah Silva. All rights reserved.</small>
         </div>
       </footer>
   )
