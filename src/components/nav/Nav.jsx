811fe2973d3d7b1e0c9ae9e6970db8ad1abfeca6
Jeremiah Silva <jeremiah.ca9@gmail.com>
2022-09-06 23:27:38 -0600

    Edited File

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './nav.css';
-import {AiOutlineHome} from 'react-icons/ai'
-import {AiOutlineUser} from 'react-icons/ai'
-import {BiBook} from 'react-icons/bi'
-import {RiServiceLine} from 'react-icons/ri'
-import {BiMessageSquareDetail} from 'react-icons/bi'
-import {BiSlideshow} from 'react-icons/bi'
-import {useState} from 'react'
-import {RiUserShared2Line} from 'react-icons/ri'
+import {AiOutlineHome} from 'react-icons/ai';
+import {AiOutlineUser} from 'react-icons/ai';
+import {BiBook} from 'react-icons/bi';
+import {RiServiceLine} from 'react-icons/ri';
+import {BiMessageSquareDetail} from 'react-icons/bi';
+import {BiSlideshow} from 'react-icons/bi';
+import {useState} from 'react';
+import {RiUserShared2Line} from 'react-icons/ri';
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
@@ -24,4 +24,4 @@ const Nav = () => {
   )
 }
 
-export default Nav
+export default Nav;
